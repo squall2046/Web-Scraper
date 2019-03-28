5ca6adf2595c3ecd713d356a0cc51d3644af3791
squall2046 <squall2046@hotmail.com>
2019-03-27 20:06:35 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,113 +1,110 @@
+$(document).ready(function () {
+    // $(document).ready(function () {
+    //     $getJSON("/new", (data) => {
+    //     })
+    // })
+    // =============== click clear News button ===============
+    $("#clearBtn").on("click", () => {
+        console.log("test");
 
-// $(document).ready(function () {
-//     $getJSON("/new", (data) => {
-//     })
-// })
+        $.ajax({
+            method: "DELETE",
+            url: "/clear"
+        })
+            .then(function (data) {
+                console.log("test");
+                window.location.reload();
+            });
+    });
 
-// =============== click clear News button ===============
-$("#clearBtn").on("click", () => {
-    console.log("test");
+    // =============== click Scrape News button ===============
+    $("#scrapeBtn").on("click", () => {
 
-    $.ajax({
-        method: "DELETE",
-        url: "/clear"
+        $.ajax({
+            method: "POST",
+            url: "/scrape"
+        })
+            .then(function (data) {
+                window.location.reload();
+            });
+    });
+
+    // =============== click save button to save news ===============
+    $(document).on("click", ".saveBtn", function () {
+        let thisId = $(this).attr("data-ObjectId");
+        let thisTitle = $(this).attr("data-Title");
+        console.log(thisId);
+
+        $.ajax({
+            method: "PUT",
+            url: "/save/" + thisId,
+            data: thisTitle
+        })
+            .then(function (data) {
+                console.log(data);
+                window.location.reload()
+            });
+    });
+
+    // =============== click remove button to un-save news ===============
+    $(document).on("click", ".unSaveBtn", function () {
+        let thisId = $(this).attr("data-ObjectId");
+        console.log(thisId);
+
+        $.ajax({
+            method: "PUT",
+            url: "/unsave/" + thisId
+        })
+            .then(function (data) {
+                console.log(data);
+                window.location.reload()
+            });
+    });
+
+    // =============== click note button to edit notes ===============
+    $(document).on("click", ".noteBtn", function () {
+        $(".popUp").fadeIn();
+
+        let thisId = $(this).attr("data-ObjectId");
+        console.log(thisId);
+
+        $.ajax({
+            method: "GET",
+            url: "/note/" + thisId
+        })
+            .then(function (data) {
+                console.log(data);
+            });
+    });
+
+    // =============== click X to close pop-up page ===============
+    $(document).on("click", ".close", function () {
+        $(".popUp").fadeOut();
     })
-        .then(function (data) {
-            console.log("test");
-            window.location.reload();
-        });
-});
 
-// =============== click Scrape News button ===============
-$("#scrapeBtn").on("click", () => {
 
-    $.ajax({
-        method: "POST",
-        url: "/scrape"
-    })
-        .then(function (data) {
-            window.location.reload();
-        });
-});
+    // =============== click note-submit button to post a new note ===============
+    $(document).on("click", ".submitNoteBtn", function () {
+        event.preventDefault();
+        let writeNote = $("#writeNote").val();
+        let thisId = $(this).attr("data-ObjectId");
+        console.log(thisId);
 
-// =============== click save button to save news ===============
-$(document).on("click", ".saveBtn", function () {
-    let thisId = $(this).attr("data-ObjectId");
-    let thisTitle = $(this).attr("data-Title");
-    console.log(thisId);
+        $.ajax({
+            method: "GET",
+            url: "/note/" + thisId,
+        })
+            .then(function (data) {
+                console.log(data);
+            });
 
-    $.ajax({
-        method: "PUT",
-        url: "/save/" + thisId,
-        data: thisTitle
-    })
-        .then(function (data) {
-            console.log(data);
-            window.location.reload()
-        });
-});
-
-// =============== click remove button to un-save news ===============
-$(document).on("click", ".unSaveBtn", function () {
-    let thisId = $(this).attr("data-ObjectId");
-    console.log(thisId);
-
-    $.ajax({
-        method: "PUT",
-        url: "/unsave/" + thisId
-    })
-        .then(function (data) {
-            console.log(data);
-            window.location.reload()
-        });
-});
-
-// =============== click note button to edit notes ===============
-$(".popUp").hide()
-$(document).on("click", ".noteBtn", function () {
-    $(".popUp").show()
-
-    let thisId = $(this).attr("data-ObjectId");
-    console.log(thisId);
-
-    $.ajax({
-        method: "GET",
-        url: "/note/" + thisId
-    })
-        .then(function (data) {
-            console.log(data);
-        });
-});
-
-// =============== click X to close pop-up page ===============
-$(document).on("click", ".close", function () {
-    $(".popUp").hide()
+        $.ajax({
+            method: "POST",
+            url: "/post/" + thisId,
+            data: writeNote
+        })
+            .then(function (data) {
+                console.log(data);
+            });
+    });
 })
-
-
-// =============== click note-submit button to post a new note ===============
-$(document).on("click", ".submitNoteBtn", function () {
-    event.preventDefault();
-    let writeNote = $("#writeNote").val();
-    let thisId = $(this).attr("data-ObjectId");
-    console.log(thisId);
-
-    $.ajax({
-        method: "GET",
-        url: "/note/" + thisId,
-    })
-        .then(function (data) {
-            console.log(data);
-
-        });
-
-    $.ajax({
-        method: "POST",
-        url: "/post/" + thisId,
-        data: writeNote
-    })
-        .then(function (data) {
-            console.log(data);
-
-        });
-});
